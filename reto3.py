601f5504a6001bb20aa5919793c2fa32c441cc95
carlos orjuela <orjuelacarlos80@gmail.com>
2024-03-10 10:52:27 -0500

    el reto 3

---
+++ reto3.py
@@ -0,0 +1,19 @@
+#reto 3
+
+#realizar una funcion que revise si 2 palabras son anagramas, no aplica si son iguales
+
+def determinar_anagrama(palabra1, palabra2):
+    if palabra1 == palabra2:
+        return False
+    lst = list(palabra1)
+    lst.sort()
+    lst2 = list(palabra2)
+    lst2.sort()
+    if len(lst) != len(lst2):
+        return False
+    for i in range(len(lst)):
+        if lst[i] != lst2[i]:
+            return False
+    return True
+
+print(determinar_anagrama("amor", "amor"))
