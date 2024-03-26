f78bd76864678529376e707c57e8ae63784c35fd
Carlos O <orjuelacarlos80@gmail.com>
2024-03-26 10:12:21 -0500

    proyecto final hecho

--- reto5.py
+++ reto5.py
@@ -2,6 +2,8 @@
 
 import sys
 import random
+import time
+import ctypes as tip
 
 global p #puestos de la tabla (se uso p por practicidad)
 global poc
@@ -11,6 +13,8 @@ p = [
     [7,8,9]
 ]
 poc = [0,1,2] #posiciones del arreglo puestos
+
+
 def print_line():
     print("+-------+-------+-------+")
 def print_space():
@@ -22,6 +26,7 @@ def print_tabla():
         print("|   "+str(p[i][0])+"   |   "+str(p[i][1])+"   |   "+str(p[i][2])+"   |")
         print_space()
     print_line()
+
     
 def endp(x): #X>com;O>usr
     if x == "X":
@@ -30,6 +35,7 @@ def endp(x): #X>com;O>usr
     else:
         print("ganaste")
         sys.exit()
+
 def determinar_ganador():
     for i in range(3):
         if p[0][i] == p[1][i] and p[1][i] == p[2][i]: #verificar lineas verticales
@@ -41,30 +47,58 @@ def determinar_ganador():
         endp(p[0][0])
     if p[0][2] == p[1][1] and p [1][1] == p[2][0]:# verificar linea desde 0,2 hasta 2,0
         endp(p[0][2])
+
+
 def jugada_com():
     while True:
         x = random.choice(poc)
         y = random.choice(poc)
-        print(p[x][y])
-        if p[x][y] != "X":
+        if p[x][y] != "X" and p[x][y] != "O":
             p[x][y] = "X"
             break
             
 
 def jugada_usr():
     print("tu turno")
-    x = int(input("Ingrese un numero del 1-9"))
-    for i in range(len(p)):
-        for y in range(len(p)):
-            if p[i][y] != "O" and p[i][y] == x:
-                p[i][y] = "O"
+    position = None
+    while True:
+        x = input("Ingrese un numero del 1-9: ")
+        if(not x.isnumeric()):
+            print("numero no valido")
+            continue
+        x = int(x)
+        
+        for i in range(len(p)):
+            for y in range(len(p)):
+                if tip.cast(position, tip.py_object) == "X":
+                    break
+                if p[i][y] != "O" and p[i][y] == x and  p[i][y] != "X":
+                    position = id(p[i][y])
+                    p[i][y] = "O"
+                    break
+        break
+        
+                
+            
+        
+        
+        
+        
+            
+        
+                    
 
 print("Bienvenido al tic-tac-toe (triki por si eres de chibchombia)")
-
+print_tabla()
 while True:
-    print_tabla()
+    time.sleep(1)
     jugada_com()
+    time.sleep(1)
     print_tabla()
-    jugada_usr()
     determinar_ganador()
-    3
+    time.sleep(1)
+    jugada_usr()
+    time.sleep(0.3)
+    print_tabla()
+    determinar_ganador()
+    
