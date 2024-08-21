33265563784e406bd87fbbbf3eebb15c044a66d0
Carlos O <orjuelacarlos80@gmail.com>
2024-08-21 08:48:01 -0500

    ejercicio electiva pygame

---
+++ electiva 3/210824 background.py
@@ -0,0 +1,39 @@
+import pygame, sys
+pygame.init()
+display_info = pygame.display.Info() 
+screen_size = (display_info.current_w, display_info.current_h)
+screen = pygame.display.set_mode(screen_size)
+clock = pygame.time.Clock()
+
+bg = pygame.image.load("electiva 3/xpwallpaper.jpg")
+bg = pygame.transform.scale(bg, screen_size)
+
+bg2 = bg
+
+bg_y = 0
+bg2_y = screen_size[1]
+
+
+
+screen.blit(bg, (0,0))
+
+pygame.display.update()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    bg_y += 1
+    bg2_y += 1
+    if bg_y == screen_size[1]:
+        bg_y = screen_size[1] * -1
+
+    if bg2_y == screen_size[1]:
+        bg2_y = screen_size[1] * -1
+
+    screen.blit(bg, (bg_y, 0))
+    screen.blit(bg2, (bg2_y, 0))
+
+    pygame.display.update()
+    
