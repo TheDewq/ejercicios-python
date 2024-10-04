1602d1f5318e2938b920404caf3846f3d67d0990
Carlos Orjuela <orjuelacarlos80@gmail.com>
2024-10-04 10:43:42 -0500

    avances en el escenario

--- electiva 3/haunting-system/main.py
+++ electiva 3/haunting-system/main.py
@@ -2,16 +2,18 @@ import pygame
 import os
 
 pygame.init()
+
+### SCREEN  SETTINGS ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
 screen_size = pygame.display.Info()
 screen = pygame.display.set_mode((screen_size.current_w, screen_size.current_h))
 clock = pygame.time.Clock()
 
-SCALE_TILE = (128, 128)
-TILE_SIZE = 128 
-CURRENT_PATH = os.getcwd()
-grass_tile_path = os.path.join(CURRENT_PATH,'haunting-system', 'src', 'tiles', 'grass.jpg')
-wall_tile_path = os.path.join(CURRENT_PATH,'haunting-system', 'src', 'tiles', 'wall.jpg')
+### ### SCENARY SETTINGS ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
 
+SCALE_TILE = (int((screen_size.current_w)/10), int((screen_size.current_w)/10))
+TILE_SIZE = int((screen_size.current_w)/10) 
+
+CURRENT_PATH = os.getcwd()
 
 grass_tile = pygame.image.load('grass.jpg')
 wall_tile = pygame.image.load('wall.jpg')
@@ -20,18 +22,18 @@ scaled_grass = pygame.transform.scale(grass_tile, SCALE_TILE)
 scaled_wall = pygame.transform.scale(wall_tile, SCALE_TILE)
 
 tile_map = [
-    [0, 1, 0, 0, 0, 0, 0, 1, 0],
-    [1, 1, 0, 0, 0, 0, 0, 1, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],    
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 0, 0, 0, 0, 0, 1, 1],
-    [0, 1, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+    [1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
 ]
 
-# Función para dibujar el mapa
 def draw_map():
     for y, row in enumerate(tile_map):
         for x, tile in enumerate(row):
@@ -39,6 +41,9 @@ def draw_map():
                 screen.blit(scaled_grass, (x * TILE_SIZE, y * TILE_SIZE))
             elif tile == 1:
                 screen.blit(scaled_wall, (x * TILE_SIZE, y * TILE_SIZE))
+
+
+### ### COLORS ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
 
 colores = {
     "rojo": (255, 0, 0),
@@ -58,14 +63,23 @@ colores = {
     "plateado": (192, 192, 192),
 }
 
-cube_x = 0
-cube_y = 0
+### ### PLAYER SETTINGS ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
+
+PLAYER_X = (screen_size.current_w // 2) - 50
+PLAYER_Y = (screen_size.current_h // 2) - 50
+PLAYER_SPEED = 5
+IS_W_PRESSED = False
+IS_S_PRESSED = False
+IS_A_PRESSED = False
+IS_D_PRESSED = False
+
 running = True
 
-map_size = (4000,4000)
+### ### bucle principal ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
 
-#bucle principal
 while running:
+
+    ### USER EVENTS
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -74,20 +88,39 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
             elif event.key == pygame.K_a:
-                cube_y -= 5
+                IS_A_PRESSED = True
             elif event.key == pygame.K_w:
-                cube_x -= 5
+                IS_W_PRESSED = True
             elif event.key == pygame.K_s:
-                cube_x += 5
+                IS_S_PRESSED = True
             elif event.key == pygame.K_d:
-                cube_y += 5
+                IS_D_PRESSED = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                IS_A_PRESSED = False
+            elif event.key == pygame.K_w:
+                IS_W_PRESSED = False
+            elif event.key == pygame.K_s:
+                IS_S_PRESSED = False
+            elif event.key == pygame.K_d:
+                IS_D_PRESSED = False
 
-    ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+    ### ### logica ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 
-    #logica
+    ### ### player move 
+    if IS_A_PRESSED:
+        print("nose")
+    if IS_D_PRESSED:
+        print("nose")
+    if IS_S_PRESSED:
+        print("nose")
+    if IS_W_PRESSED:
+        print("nose")
+
     screen.fill(colores["negro" ])
     draw_map()
-    pygame.draw.rect(screen, colores["rojo"], [cube_x, cube_y, 100, 100])
+    print(PLAYER_X, PLAYER_Y)
+    pygame.draw.rect(screen, colores["rojo"], [PLAYER_X, PLAYER_Y , 100, 100])
 
 
     
